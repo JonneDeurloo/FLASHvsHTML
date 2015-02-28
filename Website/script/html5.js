bbbb0bc16fb7f7bf29437553c3986bd1131be1a6
Jonne Deurloo <jonnemdeurloo@gmail.com>
2015-02-28 11:25:24 +0100

    Verwijderen van onnodige dingen

--- Website/script/html5.js
+++ Website/script/html5.js
@@ -2,10 +2,10 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var toLeft = $("#content_image").position().left + $("#content_image").width() + 15;
-    
+    $("#arrowup").css({"left": toLeft});
+
     // show the arrowup if the page is below the menu; or hide it when it is above
 	if (scroll > $("#top_menu").offset().top){
-		$("#arrowup").css({"left": toLeft});
 		$("#arrowup").show(250);
 	} else {
 		$("#arrowup").hide(250);
